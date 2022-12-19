96714ffc90f688ca59b5f78dcfbfa3a47f77a6bb
Tadesse Jemal <tadessejemal63@gmail.com>
2022-12-19 22:56:28 +0300

    fix-linter

--- Gemfile
+++ Gemfile
@@ -47,7 +47,7 @@ gem 'bootsnap', require: false
 
 # Devise
 gem 'devise'
-# authorization 
+# authorization
 gem 'cancancan'
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
-    factory :user do
-      name { 'Abebe' }
-      email { 'abebe@mail.com' }
-      password { '123456' }
-      password_confirmation { '123456' }
-  
-      trait :confirmed do
-        confirmed_at { Time.zone.now }
-        confirmation_sent_at { Time.zone.now }
-        confirmation_token { '12345' }
-      end
+  factory :user do
+    name { 'Abebe' }
+    email { 'abebe@mail.com' }
+    password { '123456' }
+    password_confirmation { '123456' }
+
+    trait :confirmed do
+      confirmed_at { Time.zone.now }
+      confirmation_sent_at { Time.zone.now }
+      confirmation_token { '12345' }
     end
+  end
 end
