9cb304af2658c6e323f528a3425d2e800f94f40b
Tadesse Jemal <tadessejemal63@gmail.com>
2022-12-20 11:22:55 +0300

    add splash

--- spec/request/splash_spec.rb
+++ spec/request/splash_spec.rb
@@ -1,7 +1,17 @@
-# RSpec.describe 'Splashes', type: :request do
-#     describe 'GET /index' do
-#       it 'returns http success' do
-#         get '/'
-#         expect(response).to have_http_status(:success)
-#     end
-# end
+require 'rails_helper'
+
+RSpec.describe 'Splashes', type: :request do
+  describe 'GET /' do
+    before :example do
+      get root_path
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'should render splash screen' do
+      expect(response).to render_template(:index)
+    end
+  end
+end
