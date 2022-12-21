7e41298d3844ec2c9e6f95e01346751e2cd9b49d
Tadesse Jemal <tadessejemal63@gmail.com>
2022-12-21 14:49:50 +0300

    add expense functionality and fix expense error

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -1,2 +1,24 @@
 module ExpensesHelper
+  def create_expense(expense, category_ids)
+    categories = []
+    category_ids.each_with_index do |category_id, idx|
+      categories << category_id unless idx.zero?
+    end
+
+    expense.transaction do
+      categories.each do |category_id|
+        @category = Category.find(category_id)
+        ExpenseCategory.save_category(@category, expense)
+      end
+    end
+  end
+
+  def category_expenses(category_id)
+    @expense_categories = ExpenseCategory.includes(:expense).where(category_id: category_id).order(created_at: :desc)
+    @expense_categories.map(&:expense)
+  end
+
+  def total_category_expense(category_id)
+    category_expenses(category_id).pluck(:amount).reduce(:+)
+  end
 end
